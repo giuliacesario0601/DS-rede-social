2deb1c6aac0b1e8d829a6c4eacf4cf2854d590e7
giuliacesario0601 <104446235+giuliacesario0601@users.noreply.github.com>
2024-10-16 08:00:27 -0300

    .

--- graficos/common.js
+++ graficos/common.js
@@ -1,5 +1,11 @@
 const getCSS = (variavel) => {
   return getComputedStyle(document.body).getPropertyValue(variavel)
-};
+}
 
-export { getCSS }
+const tickConfig = {
+  color: getCSS('--primary-color'),
+  size: 16,
+  family: getCSS('--font')
+}
+
+import { getCSS, tickConfig } from "./common.js"
